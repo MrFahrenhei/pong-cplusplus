9be325e747fa919f984021c24d5c83f47ae170e4
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2024-12-02 20:23:40 -0300

    removendo inútil

--- build/CMakeFiles/game.dir/Ball.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/game.dir/Ball.cpp.o: \
- /home/vvberaldo/CLionProjects/game/Ball.cpp /usr/include/stdc-predef.h \
- /home/vvberaldo/CLionProjects/game/Ball.h /usr/local/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/12/include/stdarg.h
